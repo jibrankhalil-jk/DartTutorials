b93a17d1fa872c56a7af0cda768cdb2c0790fcd4
Jibran Khalil <78476943+jibrankhalil-jk@users.noreply.github.com>
2021-08-20 23:48:05 +0500

    Add files via upload

---
+++ 2 # data types.dart
@@ -0,0 +1,22 @@
+
+void main(List<String> arguments) {
+
+	// Numbers: int
+	int score = 23;
+
+	// Numbers: double
+	double percentage = 93.4;
+
+	// Strings
+	var company = "Dev jk";
+
+	// Boolean
+	bool isValid = true;
+	
+	//List 
+	List fruits = ["Apple","Orange","Mango"];
+		
+	//Maps
+	Map weekDays = {'Day1': 'Mon', 'Day2': 'Tue', 'Day3': 'Wed', 'Day4': 'Thu'};	
+	
+}
---
+++ 3# variables and constants.dart
@@ -0,0 +1,23 @@
+
+void main() {
+
+
+
+
+// Variable is declared using two ways
+
+	// var
+	var cityName = 'gilgit';
+	// datatype name
+	String name = "Dev jk";	
+	
+
+
+// consttant is declared using two ways
+
+	// final
+	final cityName = 'gilgit';
+	// const
+	const name = "Dev jk";	
+	
+}
